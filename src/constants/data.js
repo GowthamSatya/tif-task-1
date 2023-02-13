a4eed183091af12807e02da8323d4d5415a74d69
GowthamSatya <gowthamsatya5@gmail.com>
2023-02-13 12:09:59 +0530

    fixed image bug

--- src/constants/data.js
+++ src/constants/data.js
@@ -1,37 +1,44 @@
+import Tomatoes from "../assets/tomatoes.png";
+import Travel from "../assets/travel.png";
+import Workout from "../assets/workout.png";
+import Corn from "../assets/corn.png";
+import Crunchwrap from "../assets/crunchwrap.png";
+import CheeseSoup from "../assets/cheese_soup.png";
+
 export const articleData = [
 	{
 		id: 1,
-		imgSrc: "src/assets/tomatoes.png",
+		imgSrc: Tomatoes,
 		title: "Grilled  Tomatoes at Home",
 		desc: "PLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard...",
 	},
 	{
 		id: 2,
-		imgSrc: "src/assets/travel.png",
+		imgSrc: Travel,
 		title: "Snacks for Travel",
 		desc: "PLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard...",
 	},
 	{
 		id: 3,
-		imgSrc: "src/assets/workout.png",
+		imgSrc: Workout,
 		title: "Post-workout Recipes",
 		desc: "PLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard...",
 	},
 	{
 		id: 4,
-		imgSrc: "src/assets/corn.png",
+		imgSrc: Corn,
 		title: "How To Grill Corn",
 		desc: "PLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard...",
 	},
 	{
 		id: 5,
-		imgSrc: "src/assets/crunchwrap.png",
+		imgSrc: Crunchwrap,
 		title: "Crunchwrap Supreme",
 		desc: "PLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard...",
 	},
 	{
 		id: 6,
-		imgSrc: "src/assets/cheese_soup.png",
+		imgSrc: CheeseSoup,
 		title: "Broccoli Cheese Soup",
 		desc: "PLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard...",
 	},
